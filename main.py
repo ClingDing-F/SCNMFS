20ddf022b540294e20d2e5c244594192bd8881c9
ClingDIng <84290076+ClingDing-F@users.noreply.github.com>
2022-07-20 12:47:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import scnmfs as scnmfs
 from utils import generate_2_class_data
 from sklearn.neighbors import KNeighborsClassifier
 
-
+import warnings
+warnings.filterwarnings("ignore")
 
 if __name__ == '__main__':
 
@@ -12,6 +13,7 @@ if __name__ == '__main__':
     scnmfs_net = scnmfs.SCNMFS(rank=2, max_iters=2000, beta=0.5, output=True, seed=1)
     scnmfs_net.fit(Data_matrix=X_train, labels=Y_train, classes=4, U_matrix=False)
     V_test = scnmfs_net.transform(Data_matrix=X_test)
-    scores = scnmfs_net.KNN_results(X_train=scnmfs_net.V_train, Y_train=Y_train, X_test=V_test, Y_test=Y_test, neighbours=10)
+    scores = scnmfs.KNN_results(X_train=scnmfs_net.V_train, Y_train=Y_train, X_test=V_test, Y_test=Y_test, neighbours=10)
     print(scores)
-    # scnmfs_net.Draw_KFold() just for 2D
+    # just for 2D
+    scnmfs.Draw_KFold(X_train,Y_train)
